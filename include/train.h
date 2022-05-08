f4175f13a806335d533a867919d03ee6e8032fc2
GabovaMasha <94718616+GabovaMasha@users.noreply.github.com>
2022-05-09 00:49:17 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -11,7 +11,7 @@ class Train {
   };
   int countOp; // счетчик шагов (число переходов из вагона в вагон)
   Cage *first; // точка входа в поезд (первый вагон)
-  Cage *last;
+  Cage *current;
 Train::Cage * create(bool light) {
 Cage* item = new Cage;
 item->light = light;
