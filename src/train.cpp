7c988f6b9c3eaefb18fa2ed4cedc9967f98a50a0
GabovaMasha <94718616+GabovaMasha@users.noreply.github.com>
2022-05-08 23:48:38 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,40 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+
+void Train::addCage(bool light) {
+if ( !(first && last)) {
+first = last = create(true);
+} else {
+last->next = create(true);
+last->next->prev = last;
+last = last->next;
+last->next = first;
+first->prev = last;
+}}
+
+int Train::getLength() {
+Cage *temp = first;
+int i = 0;
+first->light = true;
+while (temp) {
+temp = temp->next;
+i++;
+countOp++;
+if (temp->light == true) {
+temp->light = false;
+break;
+}
+}
+for (int j = 0; j < i; j++) {
+temp = temp->prev;
+countOp++;
+}
+if (!temp->light) {
+return i;
+}
+return getLength();
+}
+
+int Train::getOpCount() {
+return countOp;
+}
